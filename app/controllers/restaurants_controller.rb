d20a1c9cbc3b91dd1bca6028cec9dd3faaab58b6
rachellogie <rachel.logie1@gmail.com>
2016-07-30 16:47:02 -0600

    WIP

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,6 @@ class RestaurantsController < ApplicationController
 
   def index
     @search_city = params[:city].capitalize
-    # r = Restaurant.where("city = ?", @search_location )
     @restaurants = Restaurant.near(@search_city + ', CO, US', 5)
   end
 

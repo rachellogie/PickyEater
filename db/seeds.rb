1d9f558031d998a95e0629927e782e9836133725
rachellogie <rachel.logie1@gmail.com>
2016-07-31 13:05:47 -0600

    Added seeds to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+r = Restaurant.new(name: 'Shine', address: "2027 13th St, Boulder, CO, 80302")
+r.save!
+
+r = Restaurant.new(name: 'Blooming Beets', address: "3303 30th St, Boulder, CO 80301")
+r.save!
+
+r = Restaurant.new(name: 'Kitchen Next Door', address: "1035 Pearl St, Boulder, CO 80302")
+r.save!
+
+r = Restaurant.new(name: 'Fresh Thymes Eatery', address: "2500 30th St #101, Boulder, CO 80301")
+r.save!
+
+r = Restaurant.new(name: 'Duo', address: "2413 W 32nd Ave, Denver, CO 80211")
+r.save!

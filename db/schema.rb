5655579f3b971e9fe5d4e434fb828210ad10f839
rachellogie <rachel.logie1@gmail.com>
2016-08-03 12:57:53 -0600

    Added seeds to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801002024) do
+ActiveRecord::Schema.define(version: 20160803184847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160801002024) do
     t.float    "latitude"
     t.float    "longitude"
     t.boolean  "gluten_free", default: false
+    t.text     "description"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,41 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-r = Restaurant.new(name: 'Shine', gluten_free: true, address: "2027 13th St, Boulder, CO, 80302")
+#Boulder
+r = Restaurant.new(name: 'Shine', gluten_free: true, address: "2027 13th St, Boulder, CO, 80302", description: "Shine Restaurant & Gathering Place in Boulder, Colorado offers local, organic, nutrient dense, delicious food. Our kitchen is 100% gluten free.")
 r.save!
 
-r = Restaurant.new(name: 'Blooming Beets', gluten_free: true, address: "3303 30th St, Boulder, CO 80301")
+r = Restaurant.new(name: 'Blooming Beets', gluten_free: true, address: "3303 30th St, Boulder, CO 80301", description: "Blooming Beets is a gluten-free, grain-free, sugar free restaurant. We serve grass fed and wild caught meats and organic vegetables from local farmers.")
 r.save!
 
-r = Restaurant.new(name: 'Kitchen Next Door', address: "1035 Pearl St, Boulder, CO 80302")
+r = Restaurant.new(name: 'Kitchen Next Door', address: "1035 Pearl St, Boulder, CO 80302", description: "Adjacent to its namesake flagship, a casual hot spot for farm-to-table comfort food & craft beer.")
 r.save!
 
-r = Restaurant.new(name: 'Fresh Thymes Eatery', gluten_free: true, address: "2500 30th St #101, Boulder, CO 80301")
+r = Restaurant.new(name: 'Fresh Thymes Eatery', gluten_free: true, address: "2500 30th St #101, Boulder, CO 80301", description: "A rustic-modern cafe offering market-driven sandwiches, salads & to-go options plus beer & wine.")
+r.save!
+
+r = Restaurant.new(name: 'Aji', address: "1601 Pearl St, Boulder, CO 80302", description: "Imaginative Pan-Latin dishes in a space dominated by art & relics from Central & South America.")
+r.save!
+
+r = Restaurant.new(name: 'Kitchen', address: "1039 Pearl St, Boulder, CO 80302", description: "Farm-to-table American food in an urban space, plus drinks from the upstairs community bar.")
+r.save!
+
+r = Restaurant.new(name: 'Centro', address: "950 Pearl St, Boulder, CO 80302", description: "Imaginative Pan-Latin fare in a happening spot with happy-hour deals & occasional live music.")
+r.save!
+
+r = Restaurant.new(name: 'Chautaquah', address: "900 Baseline Rd, Boulder, CO 80302", description: "1898 tradition known for Flatiron views from the wraparound porch, plus classic American cooking.")
+r.save!
+
+r = Restaurant.new(name: 'Leaf', address: "2010 16th St, Boulder, CO 80302", description: "Hip, compact locale that features eclectic vegetarian, vegan & gluten-free dishes plus organic wine.")
+r.save!
+
+#Denver
+r = Restaurant.new(name: 'Linger', address: "2030 W 30th Ave, Denver, CO 80211", description: "This old mortuary is now a lively restaurant with a rooftop lounge & global small plates.")
 r.save!
 
 r = Restaurant.new(name: 'Duo', address: "2413 W 32nd Ave, Denver, CO 80211")
 r.save!
 
-
-#find real addresses
-r = Restaurant.new(name: 'Aji', address: "1035 Pearl St, Boulder, CO 80302")
-r.save!
-
-r = Restaurant.new(name: 'Kitchen', address: "1035 Pearl St, Boulder, CO 80302")
-r.save!
-
-r = Restaurant.new(name: 'Centro', address: "1035 Pearl St, Boulder, CO 80302")
-r.save!
-
-r = Restaurant.new(name: 'Chautaquah', address: "1035 Pearl St, Boulder, CO 80302")
-r.save!
-
-r = Restaurant.new(name: 'Greens', address: "1035 Pearl St, Boulder, CO 80302")
+r = Restaurant.new(name: 'Root Down', address: "1600 W 33rd Ave, Denver, CO 80211", description: "A converted filling station is an artful, high-energy venue for creative American small plates.")
 r.save!
 
